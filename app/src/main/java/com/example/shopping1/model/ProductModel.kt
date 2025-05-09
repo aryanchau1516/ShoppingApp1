4de3e51c39d97598a76a24215ccb751df38a670e
aryanchau1516 <aryanchau1516@gmail.com>
2025-05-09 14:32:04 +0530

    added products in categories

---
+++ app/src/main/java/com/example/shopping1/model/ProductModel.kt
@@ -0,0 +1,14 @@
+package com.example.shopping1.model
+
+import android.icu.text.CaseMap
+
+data class ProductModel(
+    val id : String = " " ,
+    val title : String = " ",
+    val description : String = " ",
+    val categories : String = " ",
+    val price : String =" ",
+    val actualPrice : String = " ",
+    val images: List<String> = emptyList()
+
+)
--- app/src/main/java/com/example/shopping1/pages/CategoriesProductPages.kt
+++ app/src/main/java/com/example/shopping1/pages/CategoriesProductPages.kt
@@ -1,11 +1,42 @@
 package com.example.shopping1.pages
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.example.shopping1.model.CategoriesModel
+import com.example.shopping1.model.ProductModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 @Composable
-fun CategoriesProductPages(modifier: Modifier = Modifier,categoryId : String) {
+fun CategoriesProductPages(modifier: Modifier = Modifier, categoryId: String) {
 
-    Text(text = "This is category page of $categoryId")
+    val productList = remember {
+        mutableStateOf<List<ProductModel>>(emptyList())
+    }
+    LaunchedEffect(key1 = Unit) {
+        Firebase.firestore.collection("data")
+            .document("stock").collection("products")
+            .whereEqualTo("categories",categoryId)
+            .get().addOnCompleteListener() {
+                if (it.isSuccessful) {
+                    val resultList = it.result.documents.mapNotNull {
+                        it.toObject(ProductModel::class.java)
+                    }
+                    productList.value = resultList
+                }
+            }
+
+    }
+
+    LazyColumn {
+        items(productList.value){
+            Text(text = it.title)
+        }
+    }
 }
