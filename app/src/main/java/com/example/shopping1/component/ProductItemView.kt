bd5f0dc8a6ef35cab0dc60c8588cb273249c07e8
aryanchau1516 <aryanchau1516@gmail.com>
2025-05-10 16:39:57 +0530

    added cart button in product list

---
+++ app/src/main/java/com/example/shopping1/component/ProductItemView.kt
@@ -0,0 +1,90 @@
+package com.example.shopping1.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.shopping1.model.ProductModel
+
+@Composable
+fun ProductItemView(modifier: Modifier, productModel: ProductModel) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        elevation = CardDefaults.cardElevation(10.dp)
+
+    ) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            AsyncImage(
+                model = productModel.images.firstOrNull(),
+                contentDescription = productModel.title,
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = productModel.title,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Row(
+                modifier = Modifier // ✅ use fresh Modifier here
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = productModel.price,
+                    color = Color.Black,
+                    fontSize = 14.sp,
+                    style = TextStyle(textDecoration = TextDecoration.LineThrough)
+                )
+                Spacer(modifier= Modifier.width(8.dp))
+                Text(
+                    text = productModel.actualPrice,
+                    color = Color.Black,
+                    fontSize = 16.sp
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = {}) {
+                  Icon(
+                      imageVector = Icons.Default.ShoppingCart,
+                      contentDescription = null
+                  )
+                }
+
+            }
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/shopping1/pages/CategoriesProductPages.kt
+++ app/src/main/java/com/example/shopping1/pages/CategoriesProductPages.kt
@@ -1,14 +1,18 @@
 package com.example.shopping1.pages
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.example.shopping1.model.CategoriesModel
+import androidx.compose.ui.unit.dp
+import com.example.shopping1.component.ProductItemView
 import com.example.shopping1.model.ProductModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -22,7 +26,7 @@ fun CategoriesProductPages(modifier: Modifier = Modifier, categoryId: String) {
     LaunchedEffect(key1 = Unit) {
         Firebase.firestore.collection("data")
             .document("stock").collection("products")
-            .whereEqualTo("categories",categoryId)
+            .whereEqualTo("categories", categoryId)
             .get().addOnCompleteListener() {
                 if (it.isSuccessful) {
                     val resultList = it.result.documents.mapNotNull {
@@ -34,9 +38,19 @@ fun CategoriesProductPages(modifier: Modifier = Modifier, categoryId: String) {
 
     }
 
-    LazyColumn {
-        items(productList.value){
-            Text(text = it.title)
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        items(productList.value.chunked(2)) {rowItems->
+            Row {
+                rowItems.forEach {
+                    ProductItemView(productModel = it, modifier = Modifier.weight(1f))
+
+                }
+                if (rowItems.size==1){
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
