b1448eb44d33bcb6962f4fbb57caa40b17be6036
aryanchau1516 <aryanchau1516@gmail.com>
2025-05-21 17:24:36 +0530

    ..

--- app/src/main/java/com/example/shopping1/pages/ProductDetailPage.kt
+++ app/src/main/java/com/example/shopping1/pages/ProductDetailPage.kt
@@ -1,6 +1,27 @@
 package com.example.shopping1.pages
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,25 +31,35 @@ import androidx.compose.ui.Modifier
 import com.example.shopping1.model.ProductModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.example.shopping1.model.CategoriesModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.tbuonomo.viewpagerdotsindicator.compose.DotsIndicator
+import com.tbuonomo.viewpagerdotsindicator.compose.model.DotGraphic
+import com.tbuonomo.viewpagerdotsindicator.compose.type.ShiftIndicatorType
 
 
 @Composable
-fun ProductDetailPage(modifier: Modifier = Modifier,productId: String) {
+fun ProductDetailPage(modifier: Modifier = Modifier, productId: String) {
 
     var product by remember { mutableStateOf(ProductModel()) }
     LaunchedEffect(key1 = Unit) {
         Firebase.firestore.collection("data")
-            .document("stock").
-            collection("products").document(productId)
+            .document("stock").collection("products").document(productId)
             .get().addOnCompleteListener() {
-                if(it.isSuccessful){
-                    val result = it .result.toObject(ProductModel::class.java)
-                    if (result!=null){
+                if (it.isSuccessful) {
+                    val result = it.result.toObject(ProductModel::class.java)
+                    if (result != null) {
                         product = result
                     }
                 }
@@ -36,6 +67,100 @@ fun ProductDetailPage(modifier: Modifier = Modifier,productId: String) {
 
     }
 
-    Text(text = product.toString())
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(
+            modifier = modifier.padding(top = 6.dp),
 
+            )
+        Text(
+            text = product.title,
+            fontWeight = FontWeight.Bold,
+
+            fontSize = 20.sp,
+            modifier = Modifier.padding(15.dp)
+        )
+
+
+        Spacer(modifier = modifier.height(8.dp))
+
+        Column {
+            val pagerState = rememberPagerState(0) {
+                product.images.size
+            }
+            HorizontalPager(state = pagerState, pageSpacing = 24.dp) {
+                AsyncImage(
+                    model = product.images.get(it), contentDescription = "",
+                    modifier = Modifier
+                        .height(220.dp)
+                        .fillMaxWidth()
+                        .padding(bottom = 3.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+            }
+            DotsIndicator(
+                dotCount = product.images.size,
+                type = ShiftIndicatorType(
+                    DotGraphic(
+                        color = MaterialTheme.colorScheme.primary,
+                        size = 6.dp
+                    )
+                ),
+
+                pagerState = pagerState
+            )
+        }
+        Spacer(modifier = modifier.height(8.dp))
+
+        Row(
+            modifier = Modifier // ✅ use fresh Modifier here
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = product.price,
+                color = Color.Black,
+                fontSize = 16.sp,
+                style = TextStyle(textDecoration = TextDecoration.LineThrough)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = product.actualPrice,
+                color = Color.Black,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null
+                )
+            }
+
+
+
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(onClick = {}, modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)) {
+
+            Text(text = "Add to cart", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text= "Product Description : ", fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = product.description, fontSize = 16.sp)
+
+
+    }
 }
